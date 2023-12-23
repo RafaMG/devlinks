1ba2d82242a29cda9e328dad254145c9fa0f0caf
Rafael M. Goncalves <rafaelmgoncalvesr@gmail.com>
2023-12-23 04:28:11 -0300

    Pequenas alterações no estilo da aplicação (cor roxa)

--- src/script.js
+++ src/script.js
@@ -17,12 +17,12 @@ function toggleMode() {
   const html = document.documentElement;
   html.classList.toggle("light");
 
-  const img = html.querySelector("#profile img");
+  // const img = html.querySelector("#profile img");
 
-  if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png");
-  } else {
-    img.setAttribute("src", "./assets/avatar.png");
-  }
+  // if (html.classList.contains("light")) {
+  //   img.setAttribute("src", "./assets/avatar-light.png");
+  // } else {
+  //   img.setAttribute("src", "./assets/avatar.png");
+  // }
 }
 
